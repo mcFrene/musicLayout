697c0356ee0a3f1c396bac76aedee38add617741
mcFrene <dkhachkov@mail.ru>
2023-11-23 00:53:44 +0300

    added adaptive

--- src/js/main.js
+++ src/js/main.js
@@ -3,4 +3,3 @@ import './parts/script-audio';
 import './parts/script-swiper';
 
 import './libs/fslightbox';
-fsLightboxInstances["gallery"].props.type = "image";
--- src/js/parts/script-swiper.js
+++ src/js/parts/script-swiper.js
@@ -5,9 +5,25 @@ const albumsSwiper = new Swiper('.albums-swiper', {
         nextEl: '.albums-wrapper-next-arrow',
         prevEl: '.albums-wrapper-prev-arrow',
     },
-    slidesPerView: 3,
     loop: true,
-    spaceBetween: 130,
+    slidesPerView: 1,
+
+    breakpoints: {
+        576: {
+            slidesPerView: 2,
+            spaceBetween: 40
+        },
+
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 80
+        },
+
+        1200: {
+            slidesPerView: 3,
+            spaceBetween: 100
+        }
+    }
 });
 
 const gallerySwiper = new Swiper('.gallery-swiper', {
